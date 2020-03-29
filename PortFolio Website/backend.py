a7f7541d59541f5fa4ef28635bcfecdc18e5143f
cianoo45 <cianoo45@gmail.com>
2020-03-29 16:32:31 +0100

    Can serve pdf resume
    The website can now serve a pdf cv on button click

--- PortFolio Website/backend.py
+++ PortFolio Website/backend.py
@@ -1,10 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,send_file
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/cv")
+def cv():
+    return send_file('static/cv.pdf')
+
 
 if __name__ == "__main__":
     app.run()
